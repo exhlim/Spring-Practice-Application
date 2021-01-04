724ce4292d2c2d755a377aee12c24286767035fa
exhlim <exhlimm@gmail.com>
2021-01-04 18:28:28 +0800

    Initial commit

---
+++ src/main/java/com/exhlim/springpracticeapp/SpringPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.exhlim.springpracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exhlim/springpracticeapp/SpringPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exhlim.springpracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
