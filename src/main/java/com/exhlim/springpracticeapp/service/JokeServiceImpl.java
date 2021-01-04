684b31d68009217f6e2993a8c1d28fed01ee4dc6
exhlim <exhlimm@gmail.com>
2021-01-04 20:01:52 +0800

    Added view layer [Displayable]

--- src/main/java/com/exhlim/springpracticeapp/service/JokeServiceImpl.java
+++ src/main/java/com/exhlim/springpracticeapp/service/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.exhlim.springpracticeapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,8 +10,8 @@ public class JokeServiceImpl implements JokeService{
 	/** Added constructor for better efficiency so there is lesser recreation of the object
 	 * 	This object will only be created once and added to the spring context
 	 * **/
-	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-		this.chuckNorrisQuotes = chuckNorrisQuotes;
+	public JokeServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
 	}
 
 	@Override
